40fd20929be85d6740e66f6cceeb41fa0bc9b309
Akshat-hype <akshatgoel310805@gmail.com>
2026-05-15 12:29:01 +0530

    rate limit set

--- backend/src/middlewares/rateLimit.middleware.js
+++ backend/src/middlewares/rateLimit.middleware.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimitMiddleware = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,                  // max 100 requests per window per IP
+  max: 10000,                  // max 10000 requests per window per IP
   standardHeaders: true,
   legacyHeaders: false,
   message: {
